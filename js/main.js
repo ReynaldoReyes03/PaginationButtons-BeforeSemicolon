d8d85b5b01935184d36e29d6626f0a954365c69a
Reynaldo Martínez Reyes <mr026812@gmail.com>
2023-04-09 12:32:04 -0500

    Add functionality to each pagination button

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ const pageNumbers = (total, max, current) => {
 };
 
 function PaginationButtons(totalPages, maxPageVisible = 10, currentPage = 1) {
-    let pages = pageNumbers(totalPages, maxPageVisible, currentPage);
-    console.log(pages);
+    let pages             = pageNumbers(totalPages, maxPageVisible, currentPage);
+    let currentPageButton = null;
 
     const buttons  = new Map();
     const fragment = document.createDocumentFragment();
@@ -33,44 +33,65 @@ function PaginationButtons(totalPages, maxPageVisible = 10, currentPage = 1) {
     const paginationButtonsContainer = document.createElement('div');
     paginationButtonsContainer.className = 'pagination-buttons';
 
-    const createAndSetupButton = (text = '', buttonClass = '', disabled = false) => {
+    const createAndSetupButton = (text = '', buttonClass = '', disabled = false, handleClick = () => {}) => {
         const button = document.createElement('button');
 
         button.textContent = text;
         button.className   = `page-btn ${buttonClass}`;
         button.disabled    = disabled;
 
+        button.addEventListener('click', e => {
+            handleClick(e);
+            this.update();
+        });
+
         return button;
     };
 
+    // Get the value of the button text and assign it to the value of the currentPage variable
+    const onPageButtonClick = e => currentPage = Number(e.currentTarget.textContent);
+
+    const onPageButtonUpdate = index => button => {
+        button.textContent = pages[index];
+
+        if (pages[index] === currentPage) {
+            currentPageButton.classList.remove('active');
+            button.classList.add('active');
+
+            currentPageButton = button;
+            currentPageButton.focus();
+        }
+    };
+
     buttons.set(
-        createAndSetupButton('start', 'start-page', disabled.start()),
-        (btn) => {}
+        createAndSetupButton('start', 'start-page', disabled.start(), () => currentPage = 1),
+        (btn) => btn.disabled = disabled.start()
     );
 
     buttons.set(
-        createAndSetupButton('prev', 'prev-page', disabled.prev()),
-        (btn) => {}
+        createAndSetupButton('prev', 'prev-page', disabled.prev(), () => currentPage -= 1),
+        (btn) => btn.disabled = disabled.prev()
     );
 
-    pages.forEach((pageNumber) => {
+    pages.forEach((pageNumber, index) => {
         const isCurrentPage = pageNumber === currentPage;
-        const button        = createAndSetupButton(pageNumber, isCurrentPage ? 'active' : '');
+        const button        = createAndSetupButton(pageNumber, isCurrentPage ? 'active' : '', false, onPageButtonClick);
 
-        buttons.set(
-            button,
-            (btn) => {}
-        )
+        if (isCurrentPage) {
+            currentPageButton = button;
+        }
+
+        buttons.set(button, onPageButtonUpdate(index))
     });
 
     buttons.set(
-        createAndSetupButton('next', 'next-page', disabled.next()),
-        (btn) => {}
+        createAndSetupButton('next', 'next-page', disabled.next(), () => currentPage += 1),
+        (btn) => btn.disabled = disabled.next()
     );
 
     buttons.set(
-        createAndSetupButton('end', 'end-page', disabled.end()),
-        (btn) => {}
+        createAndSetupButton('end', 'end-page', disabled.end(), () => currentPage = totalPages),
+        (btn) => btn.disabled = disabled.end()
     );
 
     // _   => value
@@ -83,6 +104,16 @@ function PaginationButtons(totalPages, maxPageVisible = 10, currentPage = 1) {
         paginationButtonsContainer.appendChild(fragment);
         container.appendChild(paginationButtonsContainer);
     };
+
+    this.update = (newPageNumber = currentPage) => {
+        currentPage = newPageNumber;
+
+        console.log('current page', currentPage);
+
+        pages = pageNumbers(totalPages, maxPageVisible, currentPage);
+
+        buttons.forEach((updateButton, button) => updateButton(button));
+    }
 }
 
 const paginationButtons = new PaginationButtons(100);
